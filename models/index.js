420c72ea0cdc535cb3e7edf3ca0d1a06bd13558b
root <ilyatrub@gmail.com>
2020-01-20 23:14:48 +0000

    modify index.js

--- models/index.js
+++ models/index.js
@@ -5,3 +5,5 @@ mongoose.set('debug', true);
 mongoose.connect("mongodb://localhost/todo-api", {useNewUrlParser: true, useUnifiedTopology: true});
 
 mongoose.Promise = Promise;
+
+module.exports.Todo = require("./todo");
