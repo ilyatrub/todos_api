44681d7dd1eeb6df069b10f13fd826b8476e5b21
root <ilyatrub@gmail.com>
2020-01-20 23:12:20 +0000

    add todo model

--- models/index.js
+++ models/index.js
@@ -3,3 +3,5 @@ var mongoose = require("mongoose");
 mongoose.set('debug', true);
 
 mongoose.connect("mongodb://localhost/todo-api", {useNewUrlParser: true, useUnifiedTopology: true});
+
+mongoose.Promise = Promise;
