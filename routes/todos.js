3f88e73470f5d4894de68a85b19217410b36c17e
root <ilyatrub@gmail.com>
2020-01-21 12:32:38 +0000

    add update route

--- routes/todos.js
+++ routes/todos.js
@@ -22,6 +22,25 @@ router.post("/", function(req, res){
 	})
 })
 
+router.get('/:todoId', function(req, res){
+	db.Todo.findById(req.params.todoId)
+	.then(function(foundTodo){
+		res.json(foundTodo);
+	})
+	.catch(function(err){
+		res.send(err);
+	})
+})
+
+router.put("/:todoId", function(req, res){
+	db.Todo.findOneAndUpdate({_id: req.params.todoId}, req.body, {new: true})
+	.then(function(todo){
+		res.json(todo);
+	})
+	.catch(function(err){
+		res.send(err);
+	})
+})
 
 
 module.exports = router;
