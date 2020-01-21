e5f713247ae7b8b46a922301fb4773c828077cd3
root <ilyatrub@gmail.com>
2020-01-21 15:51:56 +0000

    add logic of adding, toggling and deleting todos

--- public/app.js
+++ public/app.js
@@ -1,13 +1,82 @@
 $(document).ready(function(){
 	$.getJSON("/api/todos")
 	.then(addTodos)
-	.catch()
+	
+	
+	$("#todoInput").keypress(function(event){
+		if (event.which == 13) {
+			createTodo();
+		}
+	})
+	
+	$(".list").on("click", "li", function(){
+		updateTodo($(this));
+	})
+	
+	$(".list").on("click", "span", function(event){
+		event.stopPropagation();
+		removeTodo($(this).parent());
+	})
 })
 
 function addTodos(todos){
 	//add todos to the page
 	todos.forEach(function(todo){
-		var newTodo = $('<li class="task">' + todo.name + '</li>');
-		$(".list").append(newTodo);
+		addTodo(todo);
+	});
+}
+
+function addTodo(todo){
+	var newTodo = $('<li class="task">' + todo.name + '<span>X</span></li>');
+	newTodo.data("id", todo._id);
+	newTodo.data("completed", todo.completed);
+	if (todo.completed) {
+		newTodo.addClass("done");
+	}
+	$(".list").append(newTodo);
+}
+
+function createTodo(){
+	// send post request to create a new todo
+	var userInput = $('#todoInput').val();
+	$.post("/api/todos", {name: userInput})
+	.then(function(newTodo){
+		$('#todoInput').val('');
+		addTodo(newTodo);
+	})
+	.catch(function(err){
+		console.log(err);
+	})
+}
+
+function removeTodo(todo){
+	var clickedId = todo.data("id");
+	var deleteUrl = "/api/todos/" + clickedId;
+	// $(this).parent().remove();
+	$.ajax({
+		method: "DELETE",
+		url: deleteUrl
+	})
+	.then(function(data){
+		todo.remove();
+	})
+	.catch(function(err){
+		console.log(err);
+	})
+}
+
+function updateTodo(todo){
+	var clickedId = todo.data("id");
+	var updateUrl = "/api/todos/" + clickedId;
+	var isDone = !todo.data("completed");
+	var updateData = {completed: isDone};
+	$.ajax({
+		method: "PUT",
+		url: updateUrl,
+		data: updateData
+	})
+	.then(function(){
+		todo.toggleClass("done");
+		todo.data("completed", isDone);
 	})
 }
