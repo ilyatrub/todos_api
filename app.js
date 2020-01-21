7089a4a949c8fa34eb84eb500531f1ad84ec4732
root <ilyatrub@gmail.com>
2020-01-21 13:06:26 +0000

    refactor code

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ var todoRoutes = require("./routes/todos");
 app.use("/api/todos", todoRoutes);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/views"));
+app.use(express.static(__dirname + "/public"));
+
 
 
 // =======================
@@ -15,7 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 // =======================
 
 app.get("/", function(req, res){
-	res.send("FUCK");
+	res.sendFile("index.html");
 })
 
 app.listen(port, function(){
