7700d9efaa5d2a669b73f5934423cc6d9d003e5b
root <ilyatrub@gmail.com>
2020-01-21 12:05:50 +0000

    add routes dir, todos.js

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require("express"),
 
 var port = process.env.PORT || 3000;
 
+var todoRoutes = require("./routes/todos");
+app.use("/api/todos", todoRoutes);
+
+
+// =======================
+// ROOT ROUTES
+// =======================
+
 app.get("/", function(req, res){
 	res.send("FUCK");
 })
